579fd35892fe5dc5d58c92d1f195d9d652438e8a
Piyush Prasad <piyushprasad121@gmail.com>
2025-03-12 15:17:22 +0530

    prime range program

--- prime_range.py
+++ prime_range.py
@@ -5,3 +5,15 @@ Example:
   For start = 10, end = 30:
   11, 13, 17, 19, 23, 29
 """
+
+
+startNo = int(input("Enter the starting no.: "))
+endNo = int(input("Enter the ending no.: "))
+
+for i in range(startNo, endNo):
+    flag = 1
+    for j in range(2, i-1):
+        if i%j == 0:
+            flag = 0
+    if flag == 1:
+        print(i)
